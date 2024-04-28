a578bd0979b213c6bc16685861f089b8a17ed779
yesseruser <67008763+yesseruser@users.noreply.github.com>
2024-04-28 12:31:40 +0200

    Update and rename rust-todo.rb to yesser-todo-cli.rb (1.0.1)

--- Formula/yesser-todo-cli.rb
+++ Formula/yesser-todo-cli.rb
@@ -1,8 +1,8 @@
-class RustTodo < Formula
+class YesserTodoCli < Formula
   desc "Todo CLI written in rust"
   homepage "https://github.com/Yesser-Studios/rust-todo"
-  url "https://github.com/Yesser-Studios/rust-todo/archive/refs/tags/1.0.0.tar.gz"
-  sha256 "d42adbad710e68e2fa2689f068e35223826d222612cd990cf743bead5fca5dff"
+  url "https://github.com/Yesser-Studios/yesser-todo-cli/archive/refs/tags/1.0.1.tar.gz"
+  sha256 "04e9d7d36237049a38e53ac2cb763dfb80e5ee82cd7c270964bfe6aeefcb4341"
   license "MIT"
 
   depends_on "rust" => :build
