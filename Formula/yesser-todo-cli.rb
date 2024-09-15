7153fb1da212f90fee55315b09f7c1e6386a969e
yesseruser <yesseruseryt@gmail.com>
2024-09-15 03:26:02 +0200

    Change bottle rebuild to 1

--- Formula/yesser-todo-cli.rb
+++ Formula/yesser-todo-cli.rb
@@ -7,7 +7,7 @@ class YesserTodoCli < Formula
 
   bottle do
     root_url "https://github.com/yesser-studios/yesser-todo-cli/releases/download/1.0.2"
-    rebuild 2
+    rebuild 1
     sha256 cellar: :any_skip_relocation, arm64_sonoma: "a10e64912ab0ebada1120778237ad573dbec96827debb44b5af728b42e89886c"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "42f5d59e071185163f157903cdaad1be196c769e6006d273344accbf0020e3cc"
   end
