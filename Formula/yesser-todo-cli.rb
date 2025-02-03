e46ac5dc8b4167250b078d333880cab34077d18f
yesseruser <yesseruseryt@gmail.com>
2025-02-03 23:31:55 +0100

    Update yesser-todo-cli to version 1.1.0
    Linux bottle still needed

--- Formula/yesser-todo-cli.rb
+++ Formula/yesser-todo-cli.rb
@@ -1,14 +1,14 @@
 class YesserTodoCli < Formula
   desc "Todo CLI written in rust"
   homepage "https://github.com/Yesser-Studios/rust-todo"
-  url "https://github.com/Yesser-Studios/yesser-todo-cli/archive/refs/tags/1.0.2.tar.gz"
-  sha256 "6f49f2bf14d2ccabb46a9d59c584089800d2c71e441904b6487c3fb5bdfb6170"
+  url "https://github.com/Yesser-Studios/yesser-todo-cli/archive/refs/tags/1.1.0.tar.gz"
+  sha256 "0d4f513299b5f875bfed2c657cd6efdf33ddeb514ecc5bd2224bdcdfb16a525f"
   license "MIT"
 
   bottle do
-    root_url "https://github.com/yesser-studios/yesser-todo-cli/releases/download/1.0.2"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "a10e64912ab0ebada1120778237ad573dbec96827debb44b5af728b42e89886c"
+    root_url "https://github.com/yesser-studios/yesser-todo-cli/releases/download/1.1.0"
+    rebuild 0
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0d18192b2ced6d0fb7113c6ed1400d11eaa2a89690bf8874c7cccfb8f0a5d712"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "42f5d59e071185163f157903cdaad1be196c769e6006d273344accbf0020e3cc"
   end
 
@@ -23,6 +23,6 @@ class YesserTodoCli < Formula
     assert_predicate bin/"todo", :executable?
 
     output = shell_output("#{bin}/todo -V")
-    assert_match "todo 1.0.0", output
+    assert_match "todo 1.1.0", output
   end
 end
