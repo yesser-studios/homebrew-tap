a8cc42630da10bcb7433155368ee5c58465036ee
yesseruser <yesseruseryt@gmail.com>
2026-01-27 23:04:56 +0100

    Add pong-by-yesser-studios

---
+++ Casks/pong-by-yesser-studios.rb
@@ -0,0 +1,14 @@
+# Documentation: https://docs.brew.sh/Cask-Cookbook
+#                https://docs.brew.sh/Adding-Software-to-Homebrew#cask-stanzas
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+cask "pong-by-yesser-studios" do
+  version "1.2.4"
+  sha256 "689286647b33763fb63bb14efc26ef30b4d28bac56ddda3c2a39f193c9d984d5"
+
+  url "https://github.com/yesser-studios/Pong/releases/download/v#{version}/mac-arm64.app.tar.gz"
+  name "pong-by-yesser-studios"
+  desc "A Pong clone made in C#"
+  homepage "https://github.com/yesser-studios/Pong"
+
+  app "Pong.app"
+end
